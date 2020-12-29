0d770613439e667ae935c7ef49f2cdf04433a651
Bartek Ficek <ficek.bartlomiej@gmail.com>
2020-12-29 13:17:53 +0100

    2 version of app ('test' and 'prod') works correctly on different Docker images

--- src/main/java/pl/bartekficek/springbootdocker/Hello.java
+++ src/main/java/pl/bartekficek/springbootdocker/Hello.java
@@ -1,13 +1,17 @@
 package pl.bartekficek.springbootdocker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Hello {
 
+    @Value("${envname}")
+    private String environmentName;
+
     @GetMapping("/hello")
     public String hello() {
-        return "Hello!";
+        return "Hello! " + environmentName;
     }
 }
